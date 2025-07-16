3c161cc23735c70487863a5bf2531e411cbdb304
Hayleen <haivcarr@espol.edu.ec>
2025-07-16 11:27:48 -0500

    fix: control de rutas de endpoints

--- src/main/java/org/vb/controller/ClienteController.java
+++ src/main/java/org/vb/controller/ClienteController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping(value = {"/clientes", "/clientes/"})
 public class ClienteController {
     private final ClienteService clienteService;
 
